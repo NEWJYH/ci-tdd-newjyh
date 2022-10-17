46dad3e547cf908810035753b4a723d5aed2d162
mrsono0 <mrsono0@gmail.com>
2022-10-17 15:31:41 +0900

    TodoForrm Submit event test ok

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
-const TodoForm = () => {
+const TodoForm = ({ onInsert }) => {
+    const [value, setValue] = useState('');
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+        // console.log(value);
+    }, []);
+    const onSubmit = useCallback(e => {
+        onInsert(value);
+        setValue('');
+        e.preventDefault(); // 새로고침 방지
+    }, [onInsert, value]);
     return (<div>
-        <form>
-            <input placeholder="할 일을 입력하세요" />
+        <form onSubmit={onSubmit}>
+            <input value={value} onChange={onChange} placeholder="할 일을 입력하세요" />
             <button type="submit">등록</button>
         </form>
     </div>);
--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import TodoForm from './TodoForm';
 
 describe('<TodoForm />', () => {
@@ -7,5 +7,33 @@ describe('<TodoForm />', () => {
         const { getByText, getByPlaceholderText } = render(<TodoForm />);
         getByPlaceholderText('할 일을 입력하세요'); // input이 있는지 확인
         getByText('등록'); // button이 있는지 확인
+    });
+
+    it('changes input', () => {
+        const { getByPlaceholderText } = render(<TodoForm />);
+        const input = getByPlaceholderText('할 일을 입력하세요');
+        fireEvent.change(input, {
+            target: {
+                value: 'TDD 배우기',
+            },
+        });
+        expect(input).toHaveAttribute('value', 'TDD 배우기');
+    });
+
+    it('calls onInsert and clears input', () => {
+        const onInsert = jest.fn();
+        const { getByText, getByPlaceholderText } = render(<TodoForm onInsert={onInsert} />);
+        const input = getByPlaceholderText('할 일을 입력하세요');
+        const button = getByText('등록');
+        // change 이벤트 발생시키기
+        fireEvent.change(input, {
+            target: {
+                value: 'TDD 배우기',
+            },
+        });
+        // 버튼 클릭시키기
+        fireEvent.click(button);
+        expect(onInsert).toBeCalledWith('TDD 배우기'); // onInsert가 'TDD 배우기' 파라미터가 호출되었는지 확인
+        expect(input).toHaveAttribute('value', ''); // input이 비워졌는지 확인
     });
 });
