b0f1fc41bf6a5ac2384027b84962c034b239f45f
mrsono0 <mrsono0@gmail.com>
2022-10-18 10:07:02 +0900

    TodoList TodoItem onToggle onRemove test ok

--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, getByText } from '@testing-library/react';
+import { render, fireEvent, getByText, getAllByAltText } from '@testing-library/react';
 import TodoList from './TodoList';
 
 describe('<TodoList />', () => {
@@ -19,5 +19,16 @@ describe('<TodoList />', () => {
         const { getByText } = render(<TodoList todos={sampleTodos} />);
         getByText(sampleTodos[0].text);
         getByText(sampleTodos[1].text);
+    });
+
+    it('calls onToggle and onRemove', () => {
+        const onToggle = jest.fn();
+        const onRemove = jest.fn();
+        const { getByText, getAllByText } = render(<TodoList todos={sampleTodos}
+            onToggle={onToggle} onRemove={onRemove} />);
+        fireEvent.click(getByText(sampleTodos[0].text));
+        expect(onToggle).toBeCalledWith(sampleTodos[0].id);
+        fireEvent.click(getAllByText('삭제')[0]);
+        expect(onRemove).toBeCalledWith(sampleTodos[0].id);
     });
 });
